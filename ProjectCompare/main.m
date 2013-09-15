dfc23cc7ca9c99574aa8b3b293b856c3d147c44d
Sixten Otto <opensource@sfko.com>
2013-09-15 08:21:26 -0700

    Initial Commit

---
+++ ProjectCompare/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ProjectCompare
+//
+//  Created by Sixten Otto on 9/15/13.
+//  Copyright (c) 2013 Sixten Otto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
